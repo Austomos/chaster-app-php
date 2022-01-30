ec77076985f2b5e5f3094bd39c02ea7b22f8e73c
Austomos <52321971+Austomos@users.noreply.github.com>
2022-01-30 21:00:10 +0100

    feat: start to add object to chaster app factory, locks and files

--- src/ChasterApp.php
+++ src/ChasterApp.php
@@ -2,57 +2,23 @@
 
 namespace ChasterApp;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\GuzzleException;
-use JsonException;
+use ChasterApp\Api\Files\Files;
 
 final class ChasterApp
 {
     private string $token;
 
+    /**
+     * @param string $apiToken
+     */
     public function __construct(string $apiToken)
     {
         $this->setToken($apiToken);
     }
 
-    /**
-     * @throws \ChasterApp\ChasterException
-     */
-    public function get(string $uri, array $options = []): array|object
+    public function files(): Files
     {
-        return $this->client('GET', $uri, $options);
-    }
-
-    /**
-     * @throws \ChasterApp\ChasterException
-     */
-    public function post(string $uri, ?array $json = null, array $options = []): array|object
-    {
-        if (is_array($json)) {
-            $options['json'] = $json;
-        }
-        return $this->client('POST', $uri, $options);
-    }
-
-    /**
-     * @throws \ChasterApp\ChasterException
-     */
-    protected function client(string $method, string $uri, array $options = []): array|object
-    {
-        $options['headers']['Authorization'] = 'Bearer ' . $this->getToken();
-        $client = new Client([
-            'base_uri' => 'https://api.chaster.app',
-        ]);
-        try {
-            $response = $client->request($method, $uri, $options);
-        } catch (GuzzleException $e) {
-            throw new ChasterException('Request failed: ' . $e->getMessage(), $e->getCode());
-        }
-        try {
-            return json_decode($response->getBody(), false, 512, JSON_THROW_ON_ERROR);
-        } catch (JsonException $e) {
-            throw new ChasterException('Json decode failed: ' . $e->getMessage(), $e->getCode());
-        }
+        return new Files($this->getToken());
     }
 
     /**
