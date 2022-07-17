33ade9169eb93c8222f3d043ca203c82d0df19bf
Austomos <52321971+Austomos@users.noreply.github.com>
2022-07-18 00:45:27 +0200

    doc: add doc into interface of shared lock category

--- src/Interfaces/Api/SharedLocksInterface.php
+++ src/Interfaces/Api/SharedLocksInterface.php
@@ -3,13 +3,59 @@
 namespace ChasterApp\Interfaces\Api;
 
 use ChasterApp\Data\Enum\SharedLockStatus;
+use ChasterApp\Interfaces\ResponseInterface;
 
 interface SharedLocksInterface
 {
-    public function get(SharedLockStatus $status = SharedLockStatus::active): object;
-    public function create(array $body): object;
-    public function find(string $sharedLockId): object;
-    public function update(string $sharedLockId, array $body): void;
-    public function archive(string $sharedLockId): void;
-    public function extensions(string $sharedLockId, array $body): void;
+    public function get(SharedLockStatus $status = SharedLockStatus::active): ResponseInterface;
+    public function create(array $body): ResponseInterface;
+    public function find(string $sharedLockId): ResponseInterface;
+    public function update(string $sharedLockId, array $body): ResponseInterface;
+    public function archive(string $sharedLockId): ResponseInterface;
+    public function extensions(string $sharedLockId, array $body): ResponseInterface;
+
+    /**
+     * Check if the shared lock is in user favorites
+     * @link https://api.chaster.app/api/#/Shared%20Locks/SharedLockFavoriteController_isFavorite
+     *
+     * @param string $sharedLockId The shared lock id
+     * @return \ChasterApp\Interfaces\ResponseInterface
+     *  {
+     *      'favorite': true
+     *  }
+     * http code: 200
+     */
+    public function isFavorite(string $sharedLockId): ResponseInterface;
+
+    /**
+     * Set a shared lock as favorite
+     * @link https://api.chaster.app/api/#/Shared%20Locks/SharedLockFavoriteController_setFavorite
+     *
+     * @param string $sharedLockId The shared lock id
+     * @return \ChasterApp\Interfaces\ResponseInterface
+     * http code: 200
+     */
+    public function addFavorite(string $sharedLockId): ResponseInterface;
+
+    /**
+     * Remove a favorite shared lock
+     * @link https://api.chaster.app/api/#/Shared%20Locks/SharedLockFavoriteController_removeFavorite
+     *
+     * @param string $sharedLockId The shared lock id
+     * @return \ChasterApp\Interfaces\ResponseInterface
+     * http code: 200
+     */
+    public function removeFavorite(string $sharedLockId): ResponseInterface;
+
+    /**
+     * Get user favorite shared locks
+     * @param array $body
+     *  {
+     *      'limit': 15,
+     *      'lastId': 'string'
+     *  }
+     * @return \ChasterApp\Interfaces\ResponseInterface
+     * http code: 201
+     */
+    public function getFavorites(array $body): ResponseInterface;
 }
