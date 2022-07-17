56193928f8dd445598cab0ab871a781d8fa4e6ee
Austomos <52321971+Austomos@users.noreply.github.com>
2022-07-18 00:46:11 +0200

    feat: add interface for request class

---
+++ src/Interfaces/RequestInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ChasterApp\Interfaces;
+
+interface RequestInterface
+{
+    public function getClient(string $uri, ?array $query = null, array $options = []): void;
+    public function postClient(string $uri, ?array $query = null, array $options = []): void;
+    public function putClient(string $uri, ?array $query = null, array $options = []): void;
+    public function deleteClient(string $uri, ?array $query = null, array $options = []): void;
+    public function patchClient(string $uri, ?array $query = null, array $options = []): void;
+    public function client(string $method, string $uri, array $options = []): void;
+
+    public function getBaseRoute(): string;
+    public function getRoute(): string;
+}
