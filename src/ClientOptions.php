41deec7306cbc0c28f7623107b53bae3d4cff3cf
Austomos <52321971+Austomos@users.noreply.github.com>
2022-07-18 19:46:25 +0200

    feat: client option, add multipart handler

--- src/ClientOptions.php
+++ src/ClientOptions.php
@@ -9,28 +9,20 @@ use JetBrains\PhpStorm\ArrayShape;
 class ClientOptions extends ArrayObject implements ClientOptionsInterface
 {
     private ArrayObject $json;
+    private ArrayObject $multipart;
     private ArrayObject $query;
 
-    public function __construct(array $json = [], array $query = [])
+    public function __construct(array $json = [], array $query = [], array $multipart = [])
     {
         $this->json = new ArrayObject($json);
         $this->query = new ArrayObject($query);
+        $this->multipart = new ArrayObject($multipart);
         parent::__construct();
     }
 
     public function setQueryValue(string $key, mixed $value): void
     {
         $this->query->offsetSet($key, $value);
-    }
-
-    public function getQuery(): array
-    {
-        return $this->query->getArrayCopy();
-    }
-
-    public function setQuery(array $query): void
-    {
-        $this->query->exchangeArray($query);
     }
 
     public function getQueryValue(string $key): mixed
@@ -51,16 +43,6 @@ class ClientOptions extends ArrayObject implements ClientOptionsInterface
     public function clearQuery(): void
     {
         $this->query->exchangeArray([]);
-    }
-
-    public function getJson(): array
-    {
-        return $this->json->getArrayCopy();
-    }
-
-    public function setJson(array $json): void
-    {
-        $this->json->exchangeArray($json);
     }
 
     public function getJsonValue(string $key): mixed
@@ -88,11 +70,71 @@ class ClientOptions extends ArrayObject implements ClientOptionsInterface
         $this->json->offsetSet($key, $value);
     }
 
-    #[ArrayShape(['json' => "array", 'query' => "array"])] public function getOptions(): array
+    #[ArrayShape([
+        'json' => "array",
+        'query' => "array",
+        'multipart' => "array"
+    ])] public function getOptions(): array
     {
         return [
-            'json' => $this->json->getArrayCopy(),
-            'query' => $this->query->getArrayCopy(),
+            'json' => $this->getJson(),
+            'query' => $this->getQuery(),
+            'multipart' => $this->getMultipart(),
         ];
+    }
+
+    public function getJson(): array
+    {
+        return $this->json->getArrayCopy();
+    }
+
+    public function setJson(array $json): void
+    {
+        $this->json->exchangeArray($json);
+    }
+
+    public function getQuery(): array
+    {
+        return $this->query->getArrayCopy();
+    }
+
+    public function setQuery(array $query): void
+    {
+        $this->query->exchangeArray($query);
+    }
+
+    public function getMultipart(): array
+    {
+        return $this->multipart->getArrayCopy();
+    }
+
+    public function setMultipart(array $multipart): void
+    {
+        $this->multipart->exchangeArray($multipart);
+    }
+
+    public function clearMultipart(): void
+    {
+        $this->multipart->exchangeArray([]);
+    }
+
+    public function getMultipartValue(string $key): mixed
+    {
+        return $this->multipart->offsetGet($key);
+    }
+
+    public function hasMultipartValue(string $key): bool
+    {
+        return $this->multipart->offsetExists($key);
+    }
+
+    public function removeMultipartValue(string $key): void
+    {
+        $this->multipart->offsetUnset($key);
+    }
+
+    public function setMultipartValue(string $key, mixed $value): void
+    {
+        $this->multipart->offsetSet($key, $value);
     }
 }
--- src/Interfaces/ClientOptionsInterface.php
+++ src/Interfaces/ClientOptionsInterface.php
@@ -2,11 +2,16 @@
 
 namespace ChasterApp\Interfaces;
 
-interface ClientOptionsInterface extends \IteratorAggregate, \ArrayAccess, \Serializable, \Countable
-{
-    public function getOptions(): array;
+use ArrayAccess;
+use Countable;
+use IteratorAggregate;
+use Serializable;
 
+interface ClientOptionsInterface extends IteratorAggregate, ArrayAccess, Serializable, Countable
+{
     public function clearJson(): void;
+
+    public function clearMultipart(): void;
 
     public function clearQuery(): void;
 
@@ -14,15 +19,25 @@ interface ClientOptionsInterface extends \IteratorAggregate, \ArrayAccess, \Seri
 
     public function getJsonValue(string $key): mixed;
 
+    public function getMultipart(): array;
+
+    public function getMultipartValue(string $key): mixed;
+
+    public function getOptions(): array;
+
     public function getQuery(): array;
 
     public function getQueryValue(string $key): mixed;
 
     public function hasJsonValue(string $key): bool;
 
+    public function hasMultipartValue(string $key): bool;
+
     public function hasQueryValue(string $key): bool;
 
     public function removeJsonValue(string $key): void;
+
+    public function removeMultipartValue(string $key): void;
 
     public function removeQueryValue(string $key): void;
 
@@ -30,7 +45,13 @@ interface ClientOptionsInterface extends \IteratorAggregate, \ArrayAccess, \Seri
 
     public function setJsonValue(string $key, mixed $value): void;
 
+    public function setMultipart(array $multipart): void;
+
+    public function setMultipartValue(string $key, mixed $value): void;
+
     public function setQuery(array $query): void;
 
     public function setQueryValue(string $key, mixed $value): void;
+
+    public function __construct(array $json = [], array $query = [], array $multipart = []);
 }
