ec3d10b25fea924bde8fbf1cffe59de34b1b297c
Austomos <52321971+Austomos@users.noreply.github.com>
2022-07-19 19:16:32 +0200

    chore: update files interface

--- src/Interfaces/Api/FilesInterface.php
+++ src/Interfaces/Api/FilesInterface.php
@@ -2,6 +2,35 @@
 
 namespace ChasterApp\Interfaces\Api;
 
+use ChasterApp\Data\Enum\StorageFileType;
+use ChasterApp\Interfaces\ResponseInterface;
+use ChasterApp\RequestBody\Files\UploadFiles;
+
 interface FilesInterface
 {
+
+    /**
+     * Find a file
+     * @link https://api.chaster.app/api/#/Files/StorageController_getFileFromKey
+     *
+     * @param string $fileKey Mandatory. The file key.
+     *
+     * @return \ChasterApp\Interfaces\ResponseInterface
+     */
+    public function find(string $fileKey): ResponseInterface;
+
+    /**
+     * Upload attachments and get an attachment token to be used in messaging and posts
+     * The attachment token expires after one hour.
+     * @link https://api.chaster.app/api/#/Files/StorageController_uploadFiles
+     *
+     * @param array|\ChasterApp\RequestBody\Files\UploadFiles $files
+     * @param string|\ChasterApp\Data\Enum\StorageFileType $type
+     *
+     * @return \ChasterApp\Interfaces\ResponseInterface
+     */
+    public function upload(
+        array|UploadFiles $files,
+        string|StorageFileType $type = StorageFileType::messaging
+    ): ResponseInterface;
 }
