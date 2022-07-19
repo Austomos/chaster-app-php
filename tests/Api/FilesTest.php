f95c410f3f03498ec52c8064a522b570515e17c6
Austomos <52321971+Austomos@users.noreply.github.com>
2022-07-19 19:20:10 +0200

    test: initiate files test class for files category

---
+++ tests/Api/FilesTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Api;
+
+use ChasterApp\Api\Files;
+use PHPUnit\Framework\TestCase;
+
+class FilesTest extends TestCase {
+
+    public function testGetBaseRoute()
+    {
+    }
+
+    public function testFind()
+    {
+    }
+
+    public function testUpload()
+    {
+    }
+
+}
