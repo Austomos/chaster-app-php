c80be39fd918b9f87c428c335e3c6a70f309240a
Austomos <52321971+Austomos@users.noreply.github.com>
2022-07-23 14:49:01 +0200

    fix: fix conversations test to avoid error and remove wrong space in exception message in request class

--- src/Request.php
+++ src/Request.php
@@ -67,7 +67,7 @@ abstract class Request implements RequestInterface
             $this->response = $this->client->request($method, $route, $options);
         } catch (GuzzleException $e) {
             throw new RequestChasterException(
-                'Request failed : ' . $e->getMessage() . ' - ' . $route,
+                'Request failed: ' . $e->getMessage() . ' - ' . $route,
                 $e->getCode(),
                 $e
             );
